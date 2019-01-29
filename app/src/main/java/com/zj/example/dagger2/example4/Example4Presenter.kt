a4979534dbbd278f7c5c4e99ffdcd6a2967144f5
zhengjiong <zhengjiong2006@gmail.com>
2019-01-29 21:44:32 +0800

    feat(example4):add annotation

--- app/src/main/java/com/zj/example/dagger2/example4/Example4Presenter.kt
+++ app/src/main/java/com/zj/example/dagger2/example4/Example4Presenter.kt
@@ -8,7 +8,7 @@ import javax.inject.Inject
  * @author 郑炯
  */
 
-
+//如果module没有提供Example4Presenter, 就会去查找标记了@inject的构造方法,如果构造方法的参数module可以提供那也可以注入该对象
 class Example4Presenter @Inject constructor(val view: Example4Contract.View) {
 
     fun loadData() {
